4b092d24b54a4e48ccd222083787d04ff34652d3
alxcancado <alxcancado@Alexs-MacBook-Pro.local>
2019-05-07 22:03:15 -0300

    Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// stateless components use functions, not classes.
+const Header = () => <header id="main-header">Satoshi Cult</header>;
+
+// if using States, create a class, same thing
+// class Header extends Component {
+//   render() {
+//     return <h1>AAAAAAAA</h1>;
+//   }
+// }
+
+export default Header;
